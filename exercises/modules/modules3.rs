8cde0a72dfa13ba79f70103af1347c2c34182fd3
yilin <1479826151@qq.com>
2025-09-20 23:40:28 +0800

    Update

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -8,7 +8,6 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // TODO: Complete this use statement
 use std::time::{SystemTime, UNIX_EPOCH};
